a7bb39dda1030865cb805033dde3e0b9b8aebf2e
Dorin Pomian <dorin.pomian@colorado.edu>
2023-03-22 11:19:26 -0600

    invoke ExtractMethod attempt

--- src/main/kotlin/com/intellij/ml/llm/template/intentions/ApplyTransformationIntention.kt
+++ src/main/kotlin/com/intellij/ml/llm/template/intentions/ApplyTransformationIntention.kt
@@ -1,11 +1,16 @@
 package com.intellij.ml.llm.template.intentions
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.ide.DataManager
+import com.intellij.lang.LanguageRefactoringSupport
+import com.intellij.lang.java.JavaLanguage
 import com.intellij.ml.llm.template.LLMBundle
 import com.intellij.ml.llm.template.models.CodexRequestProvider
 import com.intellij.ml.llm.template.models.LLMRequestProvider
 import com.intellij.ml.llm.template.models.sendEditRequest
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
@@ -17,11 +22,16 @@ import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.PsiUtilBase
+import com.intellij.refactoring.JavaRefactoringActionHandlerFactory
+import com.intellij.refactoring.extractMethod.ExtractMethodProcessor
+import com.intellij.refactoring.extractMethod.PrepareFailedException
+
 
 @Suppress("UnstableApiUsage")
 abstract class ApplyTransformationIntention(
@@ -35,15 +45,68 @@ abstract class ApplyTransformationIntention(
         return editor != null && file != null
     }
 
+    private fun findSelectedPsiElements(editor: Editor, file: PsiFile?): Array<PsiElement> {
+        val selectionModel = editor.selectionModel
+        val startOffset = selectionModel.selectionStart
+        val endOffset = selectionModel.selectionEnd
+
+        val startElement = file?.findElementAt(startOffset)
+        val endElement = file?.findElementAt(if (endOffset > 0) endOffset - 1 else endOffset)
+
+        if (startElement == null || endElement == null) {
+            return emptyArray()
+        }
+
+        val commonParent = PsiTreeUtil.findCommonParent(startElement, endElement)
+        if (commonParent == null) {
+            return emptyArray()
+        }
+
+        val selectedElements = PsiTreeUtil.findChildrenOfType(commonParent, PsiElement::class.java)
+        return selectedElements.filter { it.textRange.startOffset >= startOffset && it.textRange.endOffset <= endOffset }.toTypedArray()
+    }
+
     override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
         if (editor == null || file == null) return
+
 
         val document = editor.document
         val selectionModel = editor.selectionModel
         val selectedText = selectionModel.selectedText
         if (selectedText != null) {
             val textRange = TextRange.create(selectionModel.selectionStart, selectionModel.selectionEnd)
-            transform(project, selectedText, editor, textRange)
+
+
+            /* invoke ExtractMethodHandler */
+            val selectedElements = findSelectedPsiElements(editor, file)
+            val extractMethodProcessor = ExtractMethodProcessor(
+                project,
+                editor,
+                selectedElements,
+                null,
+                "extractedMethod",
+                "",
+                "Extracted method"
+            )
+
+            try {
+//                 Perform the extraction
+                if (extractMethodProcessor.prepare()) {
+                    CommandProcessor.getInstance().executeCommand(project, {
+                        ApplicationManager.getApplication().runWriteAction {
+                            extractMethodProcessor.doRefactoring()
+                        }
+                    }, "Extract Method", null)
+                }
+            } catch (e: PrepareFailedException) {
+                // Handle the error if the extraction is not possible
+                e.message?.let { showErrorHint(project, editor, it) }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+//            transform(project, selectedText, editor, textRange)
         } else {
             val namedElement = getParentNamedElement(editor)
             if (namedElement != null) {
@@ -57,6 +120,10 @@ abstract class ApplyTransformationIntention(
                 transform(project, document.getText(textRange), editor, textRange)
             }
         }
+    }
+
+    private fun showErrorHint(project: Project, editor: Editor, message: String) {
+        com.intellij.openapi.ui.Messages.showErrorDialog(project, message, "Extract Method Error")
     }
 
     private fun getLineTextRange(document: Document, editor: Editor): TextRange {
@@ -73,6 +140,7 @@ abstract class ApplyTransformationIntention(
 
     private fun transform(project: Project, text: String, editor: Editor, textRange: TextRange) {
         val instruction = getInstruction(project, editor) ?: return
+
         logger.info("Invoke transformation action with '$instruction' instruction for '$text'")
         val task =
             object : Task.Backgroundable(project, LLMBundle.message("intentions.request.background.process.title")) {
